c204c7dbcb41654435c43ba48e241dedcb8ef389
padliyulian23 <48995424+padliyulian23@users.noreply.github.com>
2019-03-27 22:08:46 +0700

    Add files via upload

---
+++ precache-manifest.3cc936f095ff56a212a0e8cead2dde16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1eef99baaa63b58b626c",
+    "url": "/static/js/main.08d830e0.chunk.js"
+  },
+  {
+    "revision": "63fbd9386df33ca80f7e",
+    "url": "/static/js/2.7369b84c.chunk.js"
+  },
+  {
+    "revision": "1eef99baaa63b58b626c",
+    "url": "/static/css/main.184e632e.chunk.css"
+  },
+  {
+    "revision": "fe7e473f6172e9bfa9826888aac7efb0",
+    "url": "/index.html"
+  }
+];
